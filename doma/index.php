5a648e12d6263210016d86774d39760fe8d51f9a
matstroeng <matstroeng@0c0cc72a-e369-11de-8ee0-a52971d33838>
2009-12-10 23:58:34 +0000

    Continuous integration test succeeded - reverting test changes

--- doma/index.php
+++ doma/index.php
@@ -10,7 +10,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>Testing continuous integration... <?php print __("PAGE_TITLE")?></title>
+  <title><?php print __("PAGE_TITLE")?></title>
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
   <link rel="icon" type="image/png" href="gfx/favicon.png" />
   <link rel="stylesheet" href="style.css" type="text/css" />
